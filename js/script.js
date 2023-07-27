bd3da00c952c729ee26635d34d874fee661e473e
Keng Vang <tomakairos@gmail.com>
2023-07-26 19:40:14 -0500

    updated radio button to + to employee results. also added in submitted logs. you can now switch between employees and it will not erase the submitted logs. logs will only be erased when refreshed as no serverside is being used. stored locally only. Because this is a project in my early development phase i will not add a server side language to this project EVER. unless requested... this was a good practice for me. I plan on recreating this project again until i get it pat down. this was very hard. for me haha.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let selectedEmployee = document.querySelector(".eresult");
 let employeeInfo = document.getElementById("employeeInfo");
 
+
 var empData;
 
 var employeeData = [
@@ -45,13 +46,15 @@ var employeeData = [
 var originalPositive;
 var originalNegative;
 
+const employeeReports = {};
+
 function displayEmployeeInfo(i) {
     empData = employeeData[i];
     originalPositive = empData.Positive;
     originalNegative = empData.Negative;
   
-    employeeInfo.innerHTML =
-      `<div id="eData">
+    employeeInfo.innerHTML = `
+    <div id="eData">
       First Name: ${empData.FirstName}<br>
       Last Name: ${empData.LastName}<br>
       Age: ${empData.Age}<br>
@@ -59,33 +62,100 @@ function displayEmployeeInfo(i) {
       Occupation: ${empData.Occupation}<br><br>
       (+)Reports: ${empData.Positive}<br>
       (-)Reports: ${empData.Negative}<br>
-      </div>
+    </div>
       <div id="rating">
         <h4>Select today's report:</h4>
-        <label for="ratingPR"><input id="ratingPR" type="radio" name="rating" value="Positive" required>Positive (+)</label>
-        <label for="ratingNR"><input id="ratingNR" type="radio" name="rating" value="negative" required>Negative (-)</label>
-            <div id="reportHere" style="display: none;">
-                <label for="textInput">Summarize your report:</label>
-                <textarea type="text" id="textInput" placeholder="Enter in your report." required> </textarea>
-                <button id="submitBtn" type="submit">Submit</button>
-            </div>
+        <label for="ratingPR">
+        <input id="ratingPR" type="radio" name="rating" value="Positive" required>Positive (+)
+        </label>
+        <label for="ratingNR">
+        <input id="ratingNR" type="radio" name="rating" value="Negative" required>Negative (-)
+        </label>
+        <div id="reportHere" style="display: none;">
+          <label for="textInput">Summarize your report:</label>
+          <textarea type="text" id="textInput" placeholder="Enter in your report." required></textarea>
+          <button id="submitBtn" type="submit">Submit</button>
         </div>
+      </div>
+      <div id="submittedLogs"></div> <!-- This is where we'll display the submitted logs -->
     `;
+
+
     const ratingPR = document.getElementById("ratingPR");
     const ratingNR = document.getElementById("ratingNR");
     const reportHere = document.getElementById("reportHere");
-
+    const submittedLogs = document.getElementById("submittedLogs");
+  
     function showReportHere() {
-        reportHere.style.display = "flex";
+      reportHere.style.display = "flex";
+    }
+
+  ratingPR.addEventListener("change", showReportHere);
+  ratingNR.addEventListener("change", showReportHere);
+
+  submittedLogs.style.display = "block";
+  submittedLogs.innerHTML = `
+    <h2>Submitted Reports for ${empData.FirstName} ${empData.LastName}</h2>
+    <ul>
+      ${employeeReports[i] ? employeeReports[i].map(
+        (report) => `
+          <li>
+            ${report.date} at ${report.time}: (${report.type}) ${report.report}
+          </li>
+        `
+      ).join('') : "No reports submitted."}
+    </ul>
+  `;
+
+  var handleSubmit = function (event) {
+    var textInput = document.getElementById("textInput");
+    var reportValue = textInput.value;
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString();
+    const formattedTime = currentDate.toLocaleTimeString();
+  
+    event.preventDefault();
+  
+    const ratingPR = document.getElementById("ratingPR");
+    const reportType = ratingPR.checked ? "Positive" : "Negative";
+  
+      if (reportType === "Positive") {
+    empData.Positive += 1;
+  } else if (reportType === "Negative") {
+    empData.Negative += 1;
+  }
+
+    if (!employeeReports[i]) {
+        employeeReports[i] = [];
       }
-    
-    ratingPR.addEventListener("change", showReportHere);
+      employeeReports[i].push({
+        date: formattedDate,
+        time: formattedTime,
+        type: reportType,
+        report: reportValue,
+      });
+  
+      submittedLogs.innerHTML = `
+        <h2>Submitted Reports for ${empData.FirstName} ${empData.LastName}</h2>
+        <ul>
+          ${employeeReports[i].map(
+            (report) => `
+            <br />
+              <li>
+                ${report.date} at ${report.time}: ${report.type === "Positive" ? "(+)" : "(-)"} ${report.report}
+              </li>
+              <br />
+              <hr />
+            `
+          ).join('')}
+        </ul>
+      `;
+    };
 
-    ratingNR.addEventListener("change", showReportHere);
-
-    const submitBtn = document.getElementById("submitBtn");
-    submitBtn.addEventListener("click", handleSubmit);
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", handleSubmit);
 }
+
 
 var selectedResult = function () {
     var selectedValue = selectedEmployee.value;
